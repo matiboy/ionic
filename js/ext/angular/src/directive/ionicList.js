ecbd10e8fd0f5af7059d68500f3e05632672a1f3
Mathieu <mathieu@redapesolutions.com>
2014-01-03 14:52:50 +0800

    Use cleaner angular.bind

--- js/ext/angular/src/directive/ionicList.js
+++ js/ext/angular/src/directive/ionicList.js
@@ -46,7 +46,8 @@ angular.module('ionic.ui.list', ['ngAnimate'])
       });
 
       // Set this item's class, first from the item directive attr, and then the list attr if item not set
-      $scope.itemClass = $scope.itemType || $parentScope.itemType;
+      var getter = $parse( $scope.itemClass );
+      $scope.parsedClass = angular.bind( $scope, getter, $scope.$parent );
 
       // Decide if this item can do stuff, and follow a certain priority 
       // depending on where the value comes from
